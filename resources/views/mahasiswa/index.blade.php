394dcf666dc4f1e3fd9b6935a2f3a4cc79f2b71e
Steven3004 <stevenjunius30@gmail.com>
2025-05-20 18:59:32 +0700

    Terbaru

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -30,10 +30,11 @@
             </div>
             </div>
             <div class="card-body">
-                 <a href="{{ route('mahasiswa.create') }}" class="btn btn-primary">Tambah</a>
+                <a href="{{ route('mahasiswa.create') }}" class="btn btn-primary">Tambah</a>
                 <table class="table">
                     <thead>
                         <tr>
+                            <th>#</th>
                             <th>NPM</th>
                             <th>Nama</th>
                             <th>Program Studi</th>
@@ -43,6 +44,7 @@
                     <tbody>
                         @foreach ($mahasiswa as $item)
                         <tr>
+                            <td><img src=" {{ asset('storage/images/'. $item->foto)  }}" width="80px" /></td>
                             <td>{{ $item->npm }}</td>
                             <td>{{ $item->nama }}</td>
                             <td>{{ $item->prodi->nama }}</td>
